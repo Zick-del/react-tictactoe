5dbc58824bfc597285c5d1cce128084e986c3391
Muhammedkulyyev Eziz (NuP1/TGA) <mue2nu@bosch.com>
2023-04-21 14:50:18 +0200

    basic functions finished

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,17 +7,21 @@ export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
 
     const onSquareClick = (i) => {
-        if (squares[i]) return
+        if (squares[i] || calculateWinner(squares)) return
+
         const nextSquare = squares.slice()
         xIsNext ? nextSquare[i] = "X" : nextSquare[i] = "0"
         setSquares(nextSquare)
         setXIsNext(!xIsNext)
     }
 
+    const winner = calculateWinner(squares)
+    let status
+    winner ? status = `Winner: ${winner}!` : status = `Next player: ${xIsNext ? "X" : "0"}`
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board">
-
                 <div className="board-row">
                     <Square value={squares[0]} onSquareClick={() => onSquareClick(0)} />
                     <Square value={squares[1]} onSquareClick={() => onSquareClick(1)} />
@@ -36,4 +40,24 @@ export default function Board() {
             </div>
         </>
     )
+}
+
+const calculateWinner = (squares) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
