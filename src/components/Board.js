0ab360a02f91124a6046dfd14991d3cafc1fe086
Muhammedkulyyev Eziz (NuP1/TGA) <mue2nu@bosch.com>
2023-04-21 14:32:07 +0200

    main functions

---
+++ src/components/Board.js
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import "./Board.css"
+import Square from "./Square"
+
+export default function Board() {
+    const [xIsNext, setXIsNext] = useState(true)
+    const [squares, setSquares] = useState(Array(9).fill(null))
+
+    const onSquareClick = (i) => {
+        if (squares[i]) return
+        const nextSquare = squares.slice()
+        xIsNext ? nextSquare[i] = "X" : nextSquare[i] = "0"
+        setSquares(nextSquare)
+        setXIsNext(!xIsNext)
+    }
+
+    return (
+        <>
+            <div className="board">
+
+                <div className="board-row">
+                    <Square value={squares[0]} onSquareClick={() => onSquareClick(0)} />
+                    <Square value={squares[1]} onSquareClick={() => onSquareClick(1)} />
+                    <Square value={squares[2]} onSquareClick={() => onSquareClick(2)} />
+                </div>
+                <div className="board-row">
+                    <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
+                    <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
+                    <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
+                </div>
+                <div className="board-row">
+                    <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
+                    <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
+                    <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
+                </div>
+            </div>
+        </>
+    )
+}
